5d6d98388fc007758091568789dc7fdf64290c38
Serj_D <serj.dub1na@gmail.com>
2020-04-20 10:16:24 -0700

    [02-Git] Add game signs input
    Add game signs: 'r', 'p', 's' input to the program
    
    Signed-off-by: Serj_D <serj.dub1na@gmail.com>

--- 02-Git/main.c
+++ 02-Git/main.c
@@ -3,7 +3,30 @@
  * @brief rock-paper-scissors game implementation in C programming language
  */
 
+#include <stdio.h>
+
 int main(void)
 {
+  char input = '\0';
+
+  puts("*** Rock paper scissors game ***\n");
+  puts("To play game enter your sign: r - for rock, p - for paper, s - for scissors");
+  puts("Press Ctrl+D or Ctrl+Z to exit the game\n");
+
+  while ((input = getchar()) != EOF)
+  {
+    switch(input)
+    {
+      case 'r':
+      break;
+
+      case 'p':
+      break;
+
+      case 's':
+      break;
+    }
+  }
+
   return 0;
 }
