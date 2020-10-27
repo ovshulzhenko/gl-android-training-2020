6bd132be7f58a10bb295a2976847ec29810697db
Kostiantyn Korhukov <kostinatyn.korzhukov@globallogic.com>
2020-10-27 14:53:44 +0200

    02-Git: add user input handling
    added implementation of getUserRespond
    
    Signed-off-by: Kostiantyn Korhukov <kostinatyn.korzhukov@globallogic.com>

--- 02-Git/game.c
+++ 02-Git/game.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define BUF_SIZE 100
 
 enum EPlayerRespond
 {
@@ -21,8 +25,36 @@ enum EPlayerRespond getComputerRespond()
 	return rand() % COUNT;
 }
 
-enum EPlayerRespond getUsersRespond()
+enum EPlayerRespond getUsersRespond(bool* continue_execution)
 {
+	char buf[BUF_SIZE];
+	char mask[50];
+	sprintf(mask, "%%%ds",BUF_SIZE -1);
+
+	while(1)
+	{
+		printf("\nrock (r) - paper (p) - scissors (s)\n\t\t\t or just (x) to finish the game\n\n>");
+		scanf(mask,buf);
+		char *s = buf;
+		while(isspace(*s) && s-buf < BUF_SIZE) ++s;
+
+	switch(tolower(*s))
+	{
+	case 'r':
+		return ROCK;
+	case 's':
+		return SCISSORS;
+	case 'p':
+		return PAPER;
+	case 'x':
+		*continue_execution = false;
+		return ROCK;
+	default:
+		printf("wrong input, try again!");
+		continue;
+	}
+	}
+
 	return ROCK;
 }
 
@@ -37,7 +69,13 @@ int main(int argc, char* argv[])
 	enum eRoundWinner winner;
 
 	computer = getComputerRespond();
-	user = getUsersRespond();
+	bool continue_execution = true;
+	user = getUsersRespond(&continue_execution);
+	if(!continue_execution)
+	{
+		printf("\n\nBye!\n");
+		return EXIT_SUCCESS;
+	}
 	winner = arbitrate(computer, user);
 
 	return EXIT_SUCCESS;
