2f33f4cebadc3cf6acba427a6a7fb4b183ccb565
Kostiantyn Korhukov <kostinatyn.korzhukov@globallogic.com>
2020-10-27 12:43:55 +0200

    02-Git: add game skeleton logic
    implemented main game functions
    Signed-off-by: Kostiantyn Korhukov <kostinatyn.korzhukov@globallogic.com>

--- 02-Git/game.c
+++ 02-Git/game.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum EPlayerRespond
+{
+	  ROCK
+	, PAPER
+	, SCISSORS
+	, COUNT
+};
+
+enum eRoundWinner
+{
+	  USER
+	, COMPUTER
+	, BOTH
+};
+
+enum EPlayerRespond getComputerRespond()
+{
+	return ROCK;
+}
+
+enum EPlayerRespond getUsersRespond()
+{
+	return ROCK;
+}
+
+enum eRoundWinner arbitrate(enum EPlayerRespond computer, enum EPlayerRespond user)
+{
+	return BOTH;
+}
 
 int main(int argc, char* argv[])
 {
+	enum EPlayerRespond computer, user;
+	enum eRoundWinner winner;
+
+	computer = getComputerRespond();
+	user = getUsersRespond();
+	winner = arbitrate(computer, user);
+
 	return EXIT_SUCCESS;
 }
